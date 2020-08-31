6fa37dab582e1276e07941efde42f376bdbd3fb1
maks4a <maks4a@gmail.com>
2020-08-31 14:00:32 +0300

    Код примера Hello World

--- Hello Word/HelloWord.dpr
+++ Hello Word/HelloWord.dpr
@@ -1,17 +1,37 @@
 ﻿program HelloWord;
 
 {$APPTYPE CONSOLE}
-
 {$R *.res}
 
 uses
+  TelegramBotApi.Client,
+  TelegramBotApi.Types,
   System.SysUtils;
+
+procedure Awesome;
+const
+  YOUR_ACCESS_TOKEN_HERE = 'YOUR_ACCESS_TOKEN_HERE';
+var
+  LBot: TTelegramBotApi;
+  LMe: TtgUser;
+begin
+  LBot := TTelegramBotApi.Create(YOUR_ACCESS_TOKEN_HERE);
+  try
+    LMe := LBot.GetMe().Result;
+    WriteLn('Hello, World! I am user ', LMe.ID, ' and my name is "', LMe.FirstName, '".');
+  finally
+    LBot.Free;
+  end;
+end;
 
 begin
   try
     { TODO -oUser -cConsole Main : Insert code here }
+    Awesome;
+    Readln
   except
     on E: Exception do
-      Writeln(E.ClassName, ': ', E.Message);
+      WriteLn(E.ClassName, ': ', E.Message);
   end;
+
 end.
